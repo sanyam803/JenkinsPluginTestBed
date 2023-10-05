b611d26c7b84ce15523831da793e7871c9d8ab38
Sanyam Vikram <sanyam.vikram1@gmail.com>
2023-10-05 09:50:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,27 @@
 provider "google" {
   project     = " My First Project"
-  #credentials = file("tcb-project-371706-b114ce01e529.json")
-  region      = "us-west1"
 
+  #credentials = file("tcb-project-371706-b114ce01e529.json")
+
+  region      = "us-west1"
 }
 
 resource "google_compute_network" "vpc1" {
+
   name                    = "my-custom-network-1"
+
   auto_create_subnetworks = "false"
 
 }
 
 resource "google_compute_subnetwork" "my-custom-subnet1" {
+
   name          = "my-custom-subnet-1"
+
   ip_cidr_range = "10.255.196.0/24"
+
   network       = google_compute_network.vpc1.name
+
   region        = "us-west1-b"
 }
 
