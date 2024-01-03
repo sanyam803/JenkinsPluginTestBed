b6caf5c71d641c250f7eb5c504af050f171c963b
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-01-03 20:24:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,19 @@
 provider "google" {
-  project     = " My First Project"
+  project     = "tf-deployer-2"
   credentials = file("creds.json")
   region      = "us-west1"
 
 }
 
 resource "google_compute_network" "vpc1" {
+  project = "tf-deployer-2"
   name                    = "my-custom-network-1"
   auto_create_subnetworks = "false"
 
 }
 
 resource "google_compute_subnetwork" "my-custom-subnet1" {
+  project = "tf-deployer-2"
   name          = "my-custom-subnet-1"
   ip_cidr_range = "10.255.196.0/24"
   network       = google_compute_network.vpc1.name
