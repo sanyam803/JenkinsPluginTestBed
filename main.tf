e281ad2545c6ed6bee8d9af0fe2b5a1ac4ac4264
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-01-05 13:52:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,18 +5,14 @@ provider "google" {
 
 }
 
-resource "google_storage_bucket" "location" {
- name          = "rohit-bucket-location"
- location      = "EU"
- storage_class = "STANDARD"
- project = "rohitproj-375209"
- uniform_bucket_level_access = false
+resource "google_compute_network" "vpc1" {
+  name                    = "mocksuccess"
+  auto_create_subnetworks = "false"
 }
 
-resource "google_storage_bucket" "class" {
- name          = "rohit-bucket-storage"
- location      = "EU"
- storage_class = "COLDLINE"
- project = "rohitproj-375209"
- uniform_bucket_level_access = false
+resource "google_compute_subnetwork" "my-custom-subnet1" {
+  name          = "my-custom-subnet-1"
+  ip_cidr_range = "10.255.196.0/24"
+  network       = google_compute_network.vpc1.name
+  region        = "us-west1-b"
 }
