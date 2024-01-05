6b6cfbc213621ac2d5692b7dd5ae8092be41b574
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-01-05 13:51:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,22 @@
 provider "google" {
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project     = "tf-deployer-2"
   credentials = file("creds.json")
-}
-
-resource "google_compute_network" "vpc1" {
-  name                    = "mocksuccess"
-  auto_create_subnetworks = "false"
+  region      = "us-west1"
 
 }
 
-resource "google_compute_subnetwork" "my-custom-subnet1" {
-  name          = "my-custom-subnet-1"
-  ip_cidr_range = "10.255.196.0/24"
-  network       = google_compute_network.vpc1.name
-  region        = "us-west1-b"
+resource "google_storage_bucket" "location" {
+ name          = "rohit-bucket-location"
+ location      = "EU"
+ storage_class = "STANDARD"
+ project = "rohitproj-375209"
+ uniform_bucket_level_access = false
 }
 
-
-
-
-#### test file to push
+resource "google_storage_bucket" "class" {
+ name          = "rohit-bucket-storage"
+ location      = "EU"
+ storage_class = "COLDLINE"
+ project = "rohitproj-375209"
+ uniform_bucket_level_access = false
+}
