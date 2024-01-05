857a5a26a801ff0fb209eb4e8bf22de22d6ad9c5
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-01-05 13:49:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,29 +4,17 @@ provider "google" {
   credentials = file("creds.json")
 }
 
-resource "google_container_node_pool" "good_node_pool" {
-  name       = "my-good-node-pool-1"
-  cluster    = "my-cluster-1"
-  project = "tf-deployer"
-  initial_node_count = 3
+resource "google_compute_network" "vpc1" {
+  name                    = "mocksuccess"
+  auto_create_subnetworks = "false"
 
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
-  }
 }
 
-
-resource "google_container_node_pool" "my_bad_node_pool_in_project1" {
-  name       = "my-bad-node-pool-1"
-  cluster    = "my-cluster-1"
-  project = "tf-deployer"
-  initial_node_count = 2
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
-  }
+resource "google_compute_subnetwork" "my-custom-subnet1" {
+  name          = "my-custom-subnet-1"
+  ip_cidr_range = "10.255.196.0/24"
+  network       = google_compute_network.vpc1.name
+  region        = "us-west1-b"
 }
 
 
