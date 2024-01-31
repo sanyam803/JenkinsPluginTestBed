1539b04fcf94e3fc4bde3f40274c0926de862d87
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-02-01 02:00:40 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -6,17 +6,25 @@ terraform {
   }
 }
 
+provider "google" {
+  project = "iac-scan-integration-test"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
 resource "google_compute_network" "bad-network"{
   name                            = "bad-network-1"
   delete_default_routes_on_create = false
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
   mtu                             = 100
+  project                         = "iac-scan-integration-test"
 }
 
 resource "google_container_node_pool" "my_bad_node_pool" {
   name               = "my-bad-node-pool-1"
   cluster            = "my-cluster-1"
+  project            = "iac-scan-integration-test"
   initial_node_count = 2
 
   node_config {
