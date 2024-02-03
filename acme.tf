962fc01a57c7f65467fe7da626387fed789f0a7f
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-02-03 20:59:02 +0530

    Update acme.tf

--- acme.tf
+++ acme.tf
@@ -11,6 +11,47 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+resource "google_storage_bucket" "my_acem_bucket_a1" {
+  name          = "my-acme-bucket-a1"
+  location      = "US"
+  force_destroy = true
+
+  project = "acme-data-ingestion-4"
+  logging {
+    log_bucket = "example-logs-bucket"
+    log_object_prefix = "log_object_prefix"
+  }
+
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "my_acem_bucket_b1" {
+  name          = "my-acme-bucket-b1"
+  location      = "US"
+  force_destroy = true
+  logging {
+    log_bucket = "example-logs-bucket"
+    log_object_prefix = "log_object_prefix"
+  }
+
+  project = "acme-data-ingestion-4"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "my_acem_bucket_c1" {
+  name          = "my-acme-bucket-c1"
+  location      = "US"
+  force_destroy = true
+
+  project = "acme-data-ingestion-4"
+  logging {
+    log_bucket = "example-logs-bucket"
+    log_object_prefix = "log_object_prefix"
+  }
+
+  uniform_bucket_level_access = true
+}
+
 
 resource "google_storage_bucket" "my_acem_bucket_a11" {
   name          = "my-acme-bucket-a11"
@@ -18,6 +59,10 @@ resource "google_storage_bucket" "my_acem_bucket_a11" {
   force_destroy = true
 
   project = "acme-data-ingestion-4"
+  logging {
+    log_bucket = "example-logs-bucket"
+    log_object_prefix = "log_object_prefix"
+  }
 
   uniform_bucket_level_access = true
 }
@@ -26,6 +71,10 @@ resource "google_storage_bucket" "my_acem_bucket_b11" {
   name          = "my-acme-bucket-b11"
   location      = "US"
   force_destroy = true
+  logging {
+    log_bucket = "example-logs-bucket"
+    log_object_prefix = "log_object_prefix"
+  }
 
   project = "acme-data-ingestion-4"
   uniform_bucket_level_access = true
@@ -37,6 +86,10 @@ resource "google_storage_bucket" "my_acem_bucket_c11" {
   force_destroy = true
 
   project = "acme-data-ingestion-4"
+  logging {
+    log_bucket = "example-logs-bucket"
+    log_object_prefix = "log_object_prefix"
+  }
 
   uniform_bucket_level_access = true
 }
