25c11dcec9ca763100a8305f5067403907601dcb
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-02-03 15:01:31 +0530

    Update acme.tf

--- acme.tf
+++ acme.tf
@@ -20,17 +20,6 @@ resource "google_compute_network" "acme-network"{
   project                         = "gceteam"
 }
 
-resource "google_container_node_pool" "my_acme_node_pool" {
-  name               = "my-acme-node-pool-1"
-  cluster            = "my-cluster-1"
-  project            = "gceteam"
-  initial_node_count = 2
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
-  }
-}
 
 resource "google_storage_bucket" "my_acem_bucket" {
   name          = "my-acme-bucket"
