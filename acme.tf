648c1de44a6492fd3ca0bf059b17bbf5a44626a0
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-02-03 20:37:10 +0530

    Update acme.tf

--- acme.tf
+++ acme.tf
@@ -12,43 +12,31 @@ provider "google" {
 }
 
 
-resource "google_storage_bucket" "my_acem_bucket_a" {
-  name          = "my-acme-bucket-a"
-  location      = "EU"
+resource "google_storage_bucket" "my_acem_bucket_a1" {
+  name          = "my-acme-bucket-a1"
+  location      = "US"
   force_destroy = true
 
   project = "acme-data-ingestion-4"
 
   uniform_bucket_level_access = true
-  logging {
-    log_bucket = "example-logs-bucket"
-    log_object_prefix = "log_object_prefix"
-  }
 }
 
-resource "google_storage_bucket" "my_acem_bucket_b" {
-  name          = "my-acme-bucket-b"
-  location      = "EU"
+resource "google_storage_bucket" "my_acem_bucket_b1" {
+  name          = "my-acme-bucket-b1"
+  location      = "US"
   force_destroy = true
 
   project = "acme-data-ingestion-4"
-  logging {
-    log_bucket = "example-logs-bucket"
-    log_object_prefix = "log_object_prefix"
-  }
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket" "my_acem_bucket_d" {
-  name          = "my-acme-bucket-d"
-  location      = "EU"
+resource "google_storage_bucket" "my_acem_bucket_c1" {
+  name          = "my-acme-bucket-c1"
+  location      = "US"
   force_destroy = true
 
   project = "acme-data-ingestion-4"
-  logging {
-    log_bucket = "example-logs-bucket"
-    log_object_prefix = "log_object_prefix"
-  }
 
   uniform_bucket_level_access = true
 }
