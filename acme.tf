2ea52db34c45f4691091ba13d3999afca6799962
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-02-15 15:01:15 +0530

    Update acme.tf

--- acme.tf
+++ acme.tf
@@ -19,45 +19,32 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_storage_bucket" "acme_bucket_a" {
-  name          = "acme_bucket_a"
+resource "google_storage_bucket" "acme_bucket_a1" {
+  name          = "acme_bucket_a1"
   location      = "US"
   force_destroy = true
   project = "acme-data-ingestion-4"
-  uniform_bucket_level_access = true
-  
-  logging {
-    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
-    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
-  }
+  uniform_bucket_level_access = false
 }
 
-resource "google_storage_bucket" "acme_bucket_b" {
-  name          = "acme_bucket_b"
+resource "google_storage_bucket" "acme_bucket_b1" {
+  name          = "acme_bucket_b1"
   location      = "US"
   force_destroy = true
   project = "acme-data-ingestion-4"
-  uniform_bucket_level_access = true
+  uniform_bucket_level_access = false
   
-  logging {
-    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
-    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
-  }
 }
 
-resource "google_storage_bucket" "acme_bucket_c" {
-  name          = "acme_bucket_c"
+resource "google_storage_bucket" "acme_bucket_c1" {
+  name          = "acme_bucket_c1"
   location      = "US"
   force_destroy = true
   
   project = "acme-data-ingestion-4"
 
-  uniform_bucket_level_access = true
-  
-  logging {
-    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
-    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
-  }
+  uniform_bucket_level_access = false
+
 }
 
 
