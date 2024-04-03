2378429034c5b0a82d53da55fb5751c4e851cad8
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-04-03 18:26:03 +0530

    Update acme.tf

--- acme.tf
+++ acme.tf
@@ -1,24 +1,15 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-    }
-  }
-}
-
 provider "google" {
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = "iac-demo-proj"
+  region  = "us-east1"
 }
 
-resource "google_storage_bucket" "acme_bucket_a3" {
-  name          = "acme_bucket_a2"
+resource "google_storage_bucket" "kishore-bucket-6" {
+  name          = "kishore-bucket-3"
   location      = "EU"
   force_destroy = true
-  project = "acme-data-ingestion-4"
-  uniform_bucket_level_access = true
   logging {
     log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
     log_object_prefix = "tf-logs/"             // Optional prefix for better structure
   } 
+  uniform_bucket_level_access = true
 }
