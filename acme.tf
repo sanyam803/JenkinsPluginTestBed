b647fe7c749ad0dcc48f8f81470a20bdcd41f7f7
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-04-04 00:01:03 +0530

    Update acme.tf

--- acme.tf
+++ acme.tf
@@ -3,14 +3,9 @@ provider "google" {
   region  = "us-east1"
 }
 
-resource "google_storage_bucket" "acme-bucket-a6" {
-  name          = "acme-bucket-a6"
+resource "google_storage_bucket" "acme-bucket-a66" {
+  name          = "acme-bucket-a66"
   location      = "EU"
-  project = "iac-demo-proj" 
   force_destroy = true
-  logging {
-    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
-    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
-  } 
   uniform_bucket_level_access = true
 }
