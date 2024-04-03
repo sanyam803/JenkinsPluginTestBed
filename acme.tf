8d7a3461bc106c158f6e248fd84f2f887aa7c50e
Sanyam Vikram <sanyam.vikram1@gmail.com>
2024-04-03 18:20:20 +0530

    Update acme.tf

--- acme.tf
+++ acme.tf
@@ -17,4 +17,8 @@ resource "google_storage_bucket" "acme_bucket_a2" {
   force_destroy = true
   project = "acme-data-ingestion-4"
   uniform_bucket_level_access = true
+  logging {
+    log_bucket   = "my-unique-logging-bucket" // Create a separate bucket for logs
+    log_object_prefix = "tf-logs/"             // Optional prefix for better structure
+  } 
 }
